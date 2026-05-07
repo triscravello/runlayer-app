785a86dea437bd01c545a6b06d9bf0c04f8aa6b1
Tristan Cravello <tlcravello@gmail.com>
2026-05-07 16:21:43 -0400

    Update WhyThisExplanation to allow a single quick reason snippet for saved outfit cards

--- components/recommendation/WhyThisExplanation.tsx
+++ components/recommendation/WhyThisExplanation.tsx
@@ -22,7 +22,7 @@ export type WhyReasonInput =
 
 export type WhyThisExplanationProps = {
   reasons?: WhyReasonInput | WhyReasonInput[];
-  maxReasons?: 2 | 3 | 4;
+  maxReasons?: 1 | 2 | 3 | 4;
   className?: string;
 };
 
