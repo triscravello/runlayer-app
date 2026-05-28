75d7970802ffc754c2ce64f2684b66841c532bac
Tristan Cravello <tlcravello@gmail.com>
2026-05-28 14:25:56 -0400

    Initial commit of middleware

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+    const token = req.cookies.get("session");
+
+    if (!token && req.nextUrl.pathname.startsWith("/admin")) {
+        return NextResponse.redirect(new URL("/auth/login"));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/admin/:path*"],
+};
